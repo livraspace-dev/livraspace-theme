e1f9e98024ae270b35800d597e08fd08749ce243
José Bellorín <dev@livraspace.com>
2024-11-13 12:26:43 -0300

    cypress test 14

--- cypress/plugins/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = (on, config) => {
-    // save all test results as a JSON file
-    // https://github.com/bahmutov/cypress-json-results
-    require('cypress-json-results')({
-      on,
-      filename: 'results.json', // default filename
-    })
-  }
